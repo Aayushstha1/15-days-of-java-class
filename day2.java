29e76afd69a10d9f5232565749f1977715fa1d64
Aayush stha <sudipk339@gmail.com>
2024-02-12 08:50:27 +0545

    guess the number

--- day2.java
+++ day2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class GuessTheNumber {
+public class day2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
