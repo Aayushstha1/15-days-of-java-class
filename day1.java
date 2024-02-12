e37089ef3fbf578c8ea716bedf191e680f1cff91
Aayush stha <sudipk339@gmail.com>
2024-02-12 08:48:18 +0545

    guess the number

--- day1.java
+++ day1.java
@@ -1,5 +1,5 @@
-public class day1 {
+public class day1{
     public static void main(String[] args) {
-      System.out.println("Hello World");
+      System.out.println("Hello World . This is MY first program in java");
     }
   }
---
+++ day2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int secretNumber = random.nextInt(100) + 1;
+        int attempts = 0;
+
+        System.out.println("Welcome to Guess the Number!");
+        System.out.println("I'm thinking of a number between 1 and 100. Can you guess it?");
+
+        while (true) {
+            try {
+                System.out.print("Enter your guess: ");
+                int guess = scanner.nextInt();
+                attempts++;
+
+                if (guess < secretNumber) {
+                    System.out.println("Too low! Try again.");
+                } else if (guess > secretNumber) {
+                    System.out.println("Too high! Try again.");
+                } else {
+                    System.out.println("Congratulations! You guessed the number " + secretNumber + " in " + attempts + " attempts!");
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Please enter a valid number.");
+                scanner.next(); // clear the scanner buffer
+            }
+        }
+
+        scanner.close();
+    }
+}
+
