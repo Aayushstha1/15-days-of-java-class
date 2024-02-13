a339b8f7d33b99e48b8f95d8610b4072ac70a351
Aayush stha <sudipk339@gmail.com>
2024-02-13 22:15:40 +0545

    Arthmetic operation

---
+++ day3.java
@@ -0,0 +1,26 @@
+public class day3 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        
+        // Addition
+        int sum = num1 + num2;
+        System.out.println("Addition: " + num1 + " + " + num2 + " = " + sum);
+        
+        // Subtraction
+        int difference = num1 - num2;
+        System.out.println("Subtraction: " + num1 + " - " + num2 + " = " + difference);
+        
+        // Multiplication
+        int product = num1 * num2;
+        System.out.println("Multiplication: " + num1 + " * " + num2 + " = " + product);
+        
+        // Division
+        int quotient = num1 / num2;
+        System.out.println("Division: " + num1 + " / " + num2 + " = " + quotient);
+        
+        // Modulus
+        int remainder = num1 % num2;
+        System.out.println("Modulus: " + num1 + " % " + num2 + " = " + remainder);
+    }
+}
