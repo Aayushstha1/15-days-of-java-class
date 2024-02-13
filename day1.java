4742a48e90cadf235c141ae7d4fa7089c07a823d
Aayush stha <sudipk339@gmail.com>
2024-02-13 22:18:23 +0545

    Basic code

--- day1.java
+++ day1.java
@@ -1,5 +1,5 @@
 public class day1{
     public static void main(String[] args) {
-      System.out.println("Hello World . This is MY first program in java");
+      System.out.println("Hello World . ");
     }
   }
