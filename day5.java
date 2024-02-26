b2b7577ac08628adbf633c5559ae1d2edb1efce4
Aayush stha <sudipk339@gmail.com>
2024-02-26 23:06:10 +0545

    string Trim()

---
+++ day5.java
@@ -0,0 +1,6 @@
+public class day5 {
+    public static void main(String[] args) {
+        System.out.println(Hello Guys);;
+    }
+    
+}
---
+++ day6.java
@@ -0,0 +1,8 @@
+public class day6 {
+
+    public static void main(String[] args) {
+        string s1= "Hello";
+        System.out.println(s1+"how are u");//without trim()
+        System.out.println(s1.trim()+"how are u");//with trim()
+    }
+}
