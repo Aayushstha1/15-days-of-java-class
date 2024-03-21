75f52e2cf6267be81499c9e40395ba106f91af6e
Aayush stha <sudipk339@gmail.com>
2024-03-21 23:21:38 +0545

    factorial

---
+++ day7.java
@@ -0,0 +1,13 @@
+public class day7 {
+    public static void main  (string[]args){
+        int x=20;
+        int y=30;
+        if (x+y<10)  {
+            System.out.println("x+y is less than 20");
+
+        } 
+        else{
+            System.out.println("x+y is less than 30");
+        }
+     }
+    }
---
+++ day8.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class day8 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to calculate its factorial: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        long factorial = calculateFactorial(number);
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+
+    public static long calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+}
+
+
